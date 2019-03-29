ed8da376683e90a4e214bc12daacc43d667eaa53
LiamZ0302 <40206481+LiamZ0302@users.noreply.github.com>
2019-03-29 17:45:12 +0800

    Add files via upload

--- img_margin.py
+++ img_margin.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import matplotlib.pylab as plt
-im = plt.imread("misaka.jpg")
+im = plt.imread("misaka0.jpg")
 print(im.shape) # 输出图像尺
 new_im = np.zeros((1600, 827, 3), np.uint8)
 new_im = new_im + 255
